15c2df9f70d8559615c9a425f2dcf31b133b9c12
binarybaron <86064887+binarybaron@users.noreply.github.com>
2024-06-18 17:37:02 +0200

    feat: Change title and add project url

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,7 @@
 export default {
-  logo: <span>My Nextra Documentation</span>,
+  logo: <span>UnstoppableSwap</span>,
   project: {
-    link: 'https://github.com/shuding/nextra'
+    link: 'https://github.com/UnstoppableSwap/unstoppableswap-docs',
   }
   // ... other theme options
 }
