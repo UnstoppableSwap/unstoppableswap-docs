055fcc5e4aef01108b29ea2ac15a393881f209e2
binarybaron <86064887+binarybaron@users.noreply.github.com>
2024-06-20 17:50:26 +0200

    fix: Provider table, prettier --write

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,8 +1,8 @@
 export default {
   logo: <span>UnstoppableSwap</span>,
   project: {
-    link: 'https://github.com/UnstoppableSwap/unstoppableswap-docs',
+    link: "https://github.com/UnstoppableSwap/unstoppableswap-docs",
   },
   primaryHue: 14.3,
   primarySaturation: 90.68,
-}
+};
